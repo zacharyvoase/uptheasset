dae2718ba996e58b03d7bc75dfaa0675e812a30f
Zachary Voase <z@zacharyvoase.com>
2010-12-30 15:19:43 +0000

    Added UTA::Models::Entry.

---
+++ lib/uta/models/entry.rb
@@ -0,0 +1,41 @@
+module UTA::Models
+  # An instance of <http://uptheasset.org/ontology#Entry>.
+  #
+  # An entry is a single credit or debit against an {Account}, which makes up
+  # part of a transaction.
+  class Entry
+    include Spira::Resource
+
+    property :type, :predicate => RDF.type
+    property :amount, :predicate => RDF::UTA.amount
+    property :account, :predicate => RDF::UTA.account, :type => :Account
+
+    # Create and save a credit against a given account.
+    #
+    # @param [RDF::Value] amount
+    #   The amount by which to credit.
+    # @param [Account, RDF::URI] account
+    #   The account to credit.
+    #
+    # @return [Entry] the created credit.
+    def self.credit(amount, account)
+      self.new(:type => RDF::UTA.Credit,
+               :amount => amount,
+               :account => account).save!
+    end
+
+    # Create and save a debit against a given account.
+    #
+    # @param [RDF::Value] amount
+    #   The amount by which to debit.
+    # @param [Account, RDF::URI] account
+    #   The account to debit.
+    #
+    # @return [Entry] the created debit.
+    def self.debit(amount, account)
+      self.new(:type => RDF::UTA.Debit,
+               :amount => amount,
+               :account => account).save!
+    end
+  end
+end
