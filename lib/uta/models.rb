858575241e7cc4576be242d689bd31aad2c84642
Zachary Voase <z@zacharyvoase.com>
2010-12-30 17:22:05 +0000

    Import all the appropriate files in UTA::Models.

--- lib/uta/models.rb
+++ lib/uta/models.rb
@@ -2,4 +2,7 @@ require 'spira'
 
 # Contains all the UpTheAsset Spira models.
 module UTA::Models
+  require 'uta/models/account'
+  require 'uta/models/entry'
+  require 'uta/models/transaction'
 end
